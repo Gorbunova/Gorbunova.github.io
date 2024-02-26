33f75b6f3bc9f887a178d1dbbd83e2902b743782
gorbunova <alinagorbunova98@mail.ru>
2024-02-26 19:41:36 +0600

    tl site pt.2

--- js/script.js
+++ js/script.js
@@ -1,8 +1,103 @@
 document.addEventListener('DOMContentLoaded', function(){
-    phoneValidate();
-
     sliderInit();
 
+    document.querySelector('.promo__header button').addEventListener('click', showModal);
+    document.querySelector('.promo__wrapper button').addEventListener('click', showModal);
+    document.querySelector('.modal__wrapper').addEventListener('click', (e) => {
+        if (!document.querySelector('.modal__inner').contains(e.target) || e.target.classList.contains('close')) hideModal();
+    });
+
+    initTypewrite();
+
+    const inputs = document.querySelectorAll('.consultation .form__input')
+    inputs.forEach(input => input.addEventListener('input', () => {
+        if (input.parentElement.classList.contains('error')) input.parentElement.classList.remove('error');
+        const allInputs = Array.from(document.querySelectorAll('.consultation  .form__input'));
+        const allInputsLengths = allInputs.map(input => input.value.length);
+        console.log(allInputsLengths)
+        if (!allInputsLengths.includes(0)) document.querySelector('.consultation__btn').removeAttribute("disabled");
+        else document.querySelector('.consultation__btn').setAttribute("disabled", true);
+    }))
+
+    const phonemask = '+7 (___) ___-__-__';
+    let phoneVal = '+7 (___) ___-__-__';
+    let inputPosition = 4;
+    const inputPhone = document.querySelector('#phone');
+    const fields = document.querySelectorAll('.form__field')
+    fields.forEach(field => field.addEventListener('click', () => {
+        field.querySelector('input').focus();
+    }))
+
+    inputPhone.addEventListener('focus', (e) => {
+        e.preventDefault();
+        inputPosition = getpos(inputPhone);
+        if (inputPhone.value === '') {
+            inputPhone.value = phonemask;
+            inputPhone.setSelectionRange(inputPosition, inputPosition)
+        }
+        else {
+            inputPosition = inputPhone.value.indexOf('_');
+            inputPhone.setSelectionRange(inputPosition, inputPosition)
+        }
+    });
+
+    inputPhone.addEventListener('click', (e) => {
+        e.preventDefault();
+        inputPosition = getpos(inputPhone);
+        if (inputPhone.value === '') {
+            inputPhone.value = phonemask;
+            inputPhone.setSelectionRange(inputPosition, inputPosition)
+        }
+        else {
+            inputPosition = inputPhone.value.indexOf('_', inputPosition);
+            inputPhone.setSelectionRange(inputPosition, inputPosition)
+        }
+    });
+
+    inputPhone.addEventListener('focusout', () => {
+        if (inputPhone.value === phonemask) inputPhone.value = '';
+    });
+
+    inputPhone.addEventListener('input', (e) => {
+        e.preventDefault();
+        console.log(e)
+        console.log(getpos(inputPhone))
+        phoneVal = phoneVal.replace('_', e.data)
+        console.log(phoneVal)
+        inputPhone.value = phoneVal
+        /* inputPhone.value = inputPhone.value.replace('_', e.data) */
+    });
+
+    document.querySelector('.consultation__btn').addEventListener('click', (e) => {
+        e.preventDefault();
+        const emailRegexp = /^\S+@\S+\.\S+$/;
+        const inputEmail = document.querySelector('#email');
+        if (!inputEmail.value.match(emailRegexp)) inputEmail.parentElement.classList.add('error');
+        else {
+            document.querySelector('.consultation__img').style.transform = 'translate(-364px, -200px) scale(0.5)';
+            setTimeout(() => {
+                document.querySelector('.consultation__img').style.opacity = '0';
+            }, 1000);
+            setTimeout(() => {
+                document.querySelector('.consultation__wrapper').innerHTML = `
+            <div class="consultation__sended">
+                <h2>Первый шаг сделан</h2>
+                <p>Скоро наш менеджер свяжется с вам для уточнения деталей</p>
+            </div>
+        `;
+            }, 2000); 
+        }
+    })
+})
+
+/* const phoneValidate = () => {
+    const phoneInput = document.querySelector('input[name=phone]');
+    phoneInput.addEventListener('input', (e) => {
+        phoneInput.value = (e.target.value.replace(/[^+ -\d]/g, ''));
+    });
+}; */
+
+const initTypewrite = () => {
     var TxtType = function(el, toRotate, period) {
         this.toRotate = toRotate;
         this.el = el;
@@ -55,20 +150,45 @@ document.addEventListener('DOMContentLoaded', function(){
               new TxtType(elements[i], JSON.parse(toRotate), period);
             }
         }
-        // INJECT CSS
-/*         var css = document.createElement("style");
-        css.type = "text/css";
-        css.innerHTML = ".typewrite > .wrap { border-right: 0.08em solid #fff}";
-        document.body.appendChild(css); */
     };
-})
+}
 
-const phoneValidate = () => {
-    const phoneInput = document.querySelector('input[name=phone]');
-    phoneInput.addEventListener('input', (e) => {
-        phoneInput.value = (e.target.value.replace(/[^+ -\d]/g, ''));
-    });
-};
+function getpos(el) {
+    var pos, sel;
+ 
+    // Internet Explorer
+    if (document.selection) {
+        el.focus();
+        if (sel=document.selection.createRange()) {
+            sel.moveStart('character', -el.value.length);
+            pos=sel.text.length;
+        }
+        // ... something wrong ...
+        else {
+            pos=0;
+        }
+    }
+    // Mozilla, Chrome
+    else if (el.selectionStart || el.selectionStart=='0') {
+        if (el.selectionDirection) {
+            if (el.selectionDirection=='backward') {
+                pos=el.selectionStart;
+            }
+            else {
+                pos=el.selectionEnd;
+            }
+        }
+        else {
+            // Opera 9
+            pos=el.selectionEnd;
+        }
+    }
+    // ... something wrong ...
+    else {
+        pos=0;
+    }
+    return parseInt(pos);
+}
 
 const sliderInit = () => {
     let sliderOffset = 0;
@@ -112,4 +232,12 @@ const sliderInit = () => {
         slider.style = `transform: translateX(-${sliderOffset}px)`;
         if ((sliderOffset + cardWidth) > maxSliderOffset) rightBtn.classList.remove(activeClass);
     });
+};
+
+const showModal = () => {
+    document.querySelector('.modal__wrapper').classList.add('active');
+};
+
+const hideModal = () => {
+    document.querySelector('.modal__wrapper').classList.remove('active');
 };
