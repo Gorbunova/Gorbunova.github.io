a17bdb4943f8220090e74ca2701bcf868decb777
gorbunova <alinagorbunova98@mail.ru>
2024-04-15 20:00:03 +0500

    tl site

--- js/script.js
+++ js/script.js
@@ -494,7 +494,8 @@ const aiAnimationInit = () => {
     const contents = document.querySelectorAll('.ai-solutions__content');
 
     const typesPadding = 30;
-    const typesGap = getStyle(document.querySelector('.ai-solutions__types'), 'gap');
+    const typesGap = getStyle(document.querySelector('.ai-solutions__types'), 'column-gap');
+    console.log(typesGap)
     const typeSize = (window.innerHeight - typesPadding * 2 - typesGap * 4) / 5;
 
     solutionsTypes.forEach(type => {
@@ -539,9 +540,9 @@ const aiAnimationInit = () => {
                 behavior: 'smooth',
             });
     }
-    window.addEventListener('scroll', throttleScroll, false);
+    window.addEventListener('scroll', (e) => scroll(e), false);
 
-    function throttleScroll(e) {
+    /* function throttleScroll(e) {
         if (isScrolling == false) {
             window.requestAnimationFrame(function () {
                 scroll(e);
@@ -549,7 +550,7 @@ const aiAnimationInit = () => {
             });
         }
         isScrolling = true;
-    }
+    } */
 
     function scroll() {
         const solutionsTop = solutions.getBoundingClientRect().top;
